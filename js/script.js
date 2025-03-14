e4923b2e1c0cb54f756bf5313448668440119017
Noah Aung <noah.tharhtetaung@gmail.com>
2025-03-14 13:39:32 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,95 +1,69 @@
 jQuery(document).ready(function($) {
     var selectedImages = [];
 
-    // Ensure loading message is hidden on page load
     $('#biatg-loading').hide();
 
-    // Open media uploader
     $('#biatg-select-images').on('click', function(e) {
         e.preventDefault();
         
         var frame = wp.media({
             title: 'Select Images',
-            button: {
-                text: 'Use Selected Images'
-            },
+            button: { text: 'Use Selected Images' },
             multiple: true,
-            library: {
-                type: 'image'
-            }
+            library: { type: 'image' }
         });
 
         frame.on('select', function() {
             var attachments = frame.state().get('selection').toJSON();
             selectedImages = attachments.map(function(attachment) {
-                return {
-                    id: attachment.id,
-                    url: attachment.url
-                };
+                return { id: attachment.id, url: attachment.url };
             });
 
-            // Display selected images
             var previewHtml = '';
             $.each(selectedImages, function(i, image) {
-                previewHtml += '<img src="' + image.url + '" style="max-width: 100px; height: auto; margin: 5px;" />';
+                previewHtml += '<img src="' + image.url + '" class="biatg-image" alt="Selected image ' + (i + 1) + '">';
             });
-            $('#biatg-image-preview').html(previewHtml);
+            $('#biatg-image-preview .biatg-placeholder').hide();
+            $('#biatg-image-preview').html(previewHtml); // Replace content to avoid duplicates
             $('#biatg-selected-images').show();
 
-            // Update image count
             $('#biatg-image-count').text('(' + selectedImages.length + ')');
-
-            // Enable the preview button
             $('#biatg-preview').prop('disabled', false);
         });
 
         frame.open();
     });
 
-    // Preview alt texts
     $('#biatg-preview').on('click', function() {
         if (selectedImages.length === 0) {
             alert('Please select some images first.');
             return;
         }
 
-        // Show loading message next to the button
-        console.log('Showing loading message next to Preview Alt Texts button...');
         $('#biatg-loading').show();
         $('#biatg-error').hide();
         $('#biatg-results').hide();
         $('#biatg-save').hide();
 
-        var image_ids = selectedImages.map(function(image) {
-            return image.id;
-        });
+        var image_ids = selectedImages.map(function(image) { return image.id; });
 
-        console.log('Starting AJAX call with image IDs:', image_ids);
-
-        // Make the AJAX call
         $.ajax({
             url: biatg_ajax.ajax_url,
             method: 'POST',
-            data: {
-                action: 'biatg_generate_alt_texts',
-                nonce: biatg_ajax.nonce,
-                image_ids: image_ids
-            },
-            beforeSend: function() {
-                console.log('AJAX request started...');
-            },
+            data: { action: 'biatg_generate_alt_texts', nonce: biatg_ajax.nonce, image_ids: image_ids },
+            beforeSend: function() { console.log('AJAX request started...'); },
             success: function(response) {
-                console.log('AJAX Success:', response);
                 $('#biatg-loading').hide();
                 if (response.success) {
                     var html = '';
                     $.each(response.data, function(i, item) {
                         html += '<tr>' +
-                            '<td><img src="' + item.url + '" style="max-width: 100px; height: auto;"></td>' +
+                            '<td><img src="' + item.url + '" class="biatg-table-image" alt="Image ' + (i + 1) + '"></td>' +
                             '<td>' + (item.current_alt || 'None') + '</td>' +
                             '<td><input type="text" class="biatg-alt-input" data-id="' + item.id + '" value="' + item.generated_alt + '"></td>' +
                             '</tr>';
                     });
+                    $('#biatg-table-body tr').hide();
                     $('#biatg-table-body').html(html);
                     $('#biatg-results').show();
                     $('#biatg-save').show();
@@ -98,26 +72,18 @@ jQuery(document).ready(function($) {
                 }
             },
             error: function(xhr, status, error) {
-                console.log('AJAX Error:', status, error);
-                console.log('Response:', xhr.responseText);
+                console.log('AJAX Error:', status, error, xhr.responseText);
                 $('#biatg-loading').hide();
                 $('#biatg-error').text('AJAX Error: ' + error + ' - ' + xhr.responseText).show();
             },
-            complete: function() {
-                console.log('AJAX call completed.');
-                $('#biatg-loading').hide();
-            }
+            complete: function() { $('#biatg-loading').hide(); }
         });
     });
 
-    // Save alt texts
     $('#biatg-save').on('click', function() {
         var alt_texts = [];
         $('.biatg-alt-input').each(function() {
-            alt_texts.push({
-                id: $(this).data('id'),
-                alt_text: $(this).val()
-            });
+            alt_texts.push({ id: $(this).data('id'), alt_text: $(this).val() });
         });
 
         $('#biatg-loading').show();
@@ -126,11 +92,7 @@ jQuery(document).ready(function($) {
         $.ajax({
             url: biatg_ajax.ajax_url,
             method: 'POST',
-            data: {
-                action: 'biatg_save_alt_texts',
-                nonce: biatg_ajax.nonce,
-                alt_texts: alt_texts
-            },
+            data: { action: 'biatg_save_alt_texts', nonce: biatg_ajax.nonce, alt_texts: alt_texts },
             success: function(response) {
                 $('#biatg-loading').hide();
                 if (response.success) {
